49990a15188a989bb8ab17ba1a673edb266589f8
PietPadda <pietergdewaal@gmail.com>
2025-02-02 11:06:10 +0200

    Finished bookbot report

---
+++ main.py
@@ -0,0 +1,128 @@
+def get_book(path):
+    """Gets a book's txt from a path.
+
+    Args:
+        string: The string path.
+
+    Returns:
+        Text of the file path (string).
+    """
+    with open(path) as f:  # with closes files after run
+        return f.read()  # get file as string
+
+
+def count_words(string):
+    """Counts the number of words in a string.
+
+    Args:
+        string: The string to analyze.
+
+    Returns:
+        The number of words in the string (int).
+    """
+    words = string.split()  # make individual words
+    return len(words)
+
+
+def count_char(string):
+    """Makes string lowercase. Then builds unique and sorted char list. Finally counts to dictionary.
+
+    Args:
+        string: The string to analyze.
+
+    Returns:
+        A dictionary with char:count pairs (dict).
+    """
+
+    # make a sorted list of chars to use in dictionary
+    lower_text = string.lower()  # make string lowercase
+    unique_characters = set()  # init empty set
+    for char in lower_text:
+        unique_characters.add(char)  # filter out uniq chars
+    sorted_unique = sorted(list(unique_characters))  # create sorted list
+
+    # count the chars
+    char_counts = {}  # init dict
+    for uniq_char in sorted_unique:
+        char_counts[uniq_char] = 0  # start empty
+        for text_char in lower_text:
+            if text_char == uniq_char:
+                char_counts[uniq_char] += 1
+
+    # print (f"DEBUG : Pre list of chars:\n\n {char_counts}\n\n")
+    return char_counts
+
+
+def alphabet_only(dict):
+    """Takes the output of count_char and filters out only alphabet.
+
+    Args:
+        dict: The dict of char:count pair.
+
+    Returns:
+        A dictionary with only alphabet filtered out  (dict).
+    """
+    alpha_dict = {}  # init dict
+    for key in dict:
+        if key.isalpha():
+            alpha_dict[key] = dict[key]
+    # print(f"DEBUG : alphabetical:\n\n {alpha_dict}\n\n")
+    return alpha_dict
+
+
+def report_dict(dict):
+    """Takes the output of alphabet_only and generates list of dicts. Use this to sort by sum ie count
+
+    Args:
+        dict: The dict of char:count pair.
+
+    Returns:
+        A list with keys sorted by descending sum  (list).
+    """
+    sum_dict = []  # init list
+
+    for name, count in dict.items():  # get key and value from dict
+        char_dict = {}  # dict per character to be added to list
+        char_dict["name"] = name  # set chardict name = key
+        char_dict["sum"] = count  # set chardict sum = value
+        sum_dict.append(char_dict)  # add each chardict to the new list
+
+    sum_dict.sort(reverse=True, key=lambda key: key["sum"])  # sort list by "sum" key in each dict inside list
+    # print(f"DEBUG : sorted dicts by sum:\n\n {sum_dict}\n\n")
+
+    return sum_dict
+
+
+def text_report(dict, word_count):
+    """Takes the output of report_dict and generates a report printout
+
+    Args:
+        list: a list of dictionaries, each corresponding to the name and count of a character
+
+    Returns:
+        A series of text for the  report  (string).
+    """
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+
+    for char_dict in dict:
+        char = char_dict["name"]
+        count = char_dict["sum"]
+        print(f"The '{char}' character was found {count} times.")
+
+    print("\n--- End report ---")
+
+
+def main():
+    """Main function to read a file, count words, and print the count."""
+    path = "books/frankenstein.txt"  # path to text
+    text = get_book(path)  # get book from path
+    word_count = count_words(text)
+    counting = count_char(text)  # create sorted dict of chars counted
+    alpha = alphabet_only(counting)  # filter out only alphabetical keys
+    rep_dict = report_dict(alpha)  # generate dict of list of dict with sorted sum
+    report = text_report(rep_dict, word_count)  # generate report to output
+
+
+if __name__ == "__main__":
+    main()
